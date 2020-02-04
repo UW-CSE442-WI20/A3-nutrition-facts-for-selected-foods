f317546d6e0b78f96ab5534fe8dd299d35eacd73
Erica Eaton <eatone3@cs.washington.edu>
2020-02-04 08:33:40 -0800

    started changing index.js to use toy csv file

--- src/index.js
+++ src/index.js
@@ -2,50 +2,66 @@ var w = 600;
 var h = 300;
 var padding = 40;
 
-var dataset = [
-  ["Sodium", 2],
-  ["Cholesterol", 1]
-];
-
 var xScale = d3.scaleBand()
   .domain(["Sodium", "Cholesterol"])
   .range([padding, w - padding])
   .paddingInner(0.1);
 
-var yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-  .range([h-padding, 0])
+var yScale = d3.scaleLinear();
 
-var xAxis = d3.axisBottom().scale(xScale);
-var yAxis = d3.axisLeft().scale(yScale);
+const csvFile = require('./data.csv');
 
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", w)
-  .attr("height", h);
+d3.csv(csvFile).then(function(data) {
 
-svg.selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("x", function(d) {
-    return xScale(d[0]);
-  })
-  .attr("y", function(d) {
-    return yScale(d[1]);
-  })
-  .attr("height", function(d) {
-    return (yScale(0) - yScale(d[1]));
-  })
-  .attr("width", 50)
-  .attr("fill", "blue");
+  var nutritionValues = data.columns.slice(1);
 
-svg.append("g")
-  .attr("class", "axis")
-  .attr("transform", "translate(0," + (h - padding) + ")")
-  .call(xAxis);
+  yScale.domain([0, d3.max(data, function(d) {
+      return d3.max(nutritionValues, function(val) { return d[val]; });
+  })])
+    .range([h-padding, 0]);
 
-svg.append("g")
-  .attr("class", "y axis")
-  .attr("transform", "translate(" + padding + ",0)")
-  .call(yAxis);
+  var xAxis = d3.axisBottom().scale(xScale);
+  var yAxis = d3.axisLeft().scale(yScale);
+
+  var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  for (var i = 0; i < nutritionValues.length; i++) {
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function(d) {
+        return xScale(nutritionValues[i]);
+      })
+      .attr("y", function(d) {
+        if (nutritionValues[i] == "Sodium") {
+          return yScale(d.Sodium);
+        } else {
+          return yScale(d.Cholesterol);
+        }
+      })
+    .attr("height", function(d) {
+      if (nutritionValues[i] == "Sodium") {
+        return (yScale(0) - yScale(d.Sodium));
+      } else {
+        return (yScale(0) - yScale(d.Cholesterol));
+      }
+    })
+    .attr("width", 50)
+    .attr("fill", "blue");
+  }
+
+  svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+});
